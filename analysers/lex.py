5bb2d99d2e7324ab1d1fbf7d4eb7b4d911ea4823
LeonardoRiveraITC <19030342@itcelaya.edu.mx>
2023-11-15 21:00:11 -0600

    Add: Estados de operador y delimitador al analisis lexico

--- analysers/lex.py
+++ analysers/lex.py
@@ -10,30 +10,40 @@ estado='a'
 alfabeto={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"}
 digito={"0","1","2","3","4","5","6","7","8","9"}
 operador={"=","+","-","/","*"}
-delimitador={'"',"(",")"}
+delimitador={"{","}","(",")",";"}
 reservadas={"si","mientras","entero","cadena","flotante"}
 
-automata=DFA({"a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p"}, #estados
+automata=DFA({"a","b","c","d","e"}, #estados
              [alfabeto,digito,operador,delimitador], #abecedario
              {
                  #transiciones 
                  #identificador
                  ("a","alfabeto"):"b",
-                 ("b","alfabetodigito"):"b",
+                 ("b","alfabeto"):"b",
+                 ("b","digito"):"b",
                  #entero o flotante
                  ("a","digito"):"c",
                  ("c","digito"):"c",
                  ("c","."):"d",
                  ("d","digito"):"e",
                  ("e","digito"):"e",
+                 #operador
+                 ("a","operador"):"f",
+                 #cadena
+                 ("a",'"'):"g",
+                 ("g","alfabeto"):"g",
+                 ("g","digito"):"g",
+                 ("g",'"'):"h",
+                 #delimitador
+                 ("a","delimitador"):"i",
                  #keywords
                  #si
-                 ("a","s"):"f",
-                 ("f","i"):"g",
-                 ("g",""):"h",
+                 #("a","s"):"f",
+                 #("f","i"):"g",
+                 #("g",""):"h",
                  #if-identificador
-                 ("f","alfabetodigito"):"b",
-                 ("g","alfabetodigito"):"b",
+                 #("f","alfabetodigito"):"b",
+                 #("g","alfabetodigito"):"b",
                  #while 
                  #("a","w"):"i",
                  #("i","h"):"j",
@@ -47,13 +57,9 @@ automata=DFA({"a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","
                  #("k","alfabetodigito"):"b",
                  #("l","alfabetodigito"):"b",
                  #("m","alfabetodigito"):"b",
-                 #cadena
-                 #("o",'"'):"p",
-                 #("p","alfabetodigito"):"q",
-                 #("q",'"'):"r",
                  },
              "a", #estado inicial
-             {"b","c","e","h","p"} #estados de aceptacion
+             {"b","c","e","h","f","i"} #estados de aceptacion
              );
 
 
