12e673baf7b0201fb44295e1b8e5cb37d91ef021
LeonardoRiveraITC <19030342@itcelaya.edu.mx>
2023-11-16 01:46:11 -0600

    Se termina generador de tabla de simbolos

--- analysers/lex.py
+++ analysers/lex.py
@@ -1,15 +1,25 @@
 from scan import limpiarWhite
 from lexDFA import * 
+from symbolTable import *
 buf=limpiarWhite("../test/read.lc")
 
-counter=0
+stable=symbolTableGlobal({})
 symbolTable={}
-estado='a'
 alfabeto={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"}
 digito={"0","1","2","3","4","5","6","7","8","9"}
 operador={"=","+","-","/","*"}
 delimitador={"{","}","(",")",";"}
 reservadas={"si","mientras","entero","cadena","flotante"}
+
+def Token(estado):
+    return {
+            'b': lambda: "id",
+            'c': lambda: "num",
+            'e': lambda: "float",
+            'f': lambda: "op",
+            'h': lambda: "cadena",
+            'i': lambda: "delim" 
+            }.get(estado, lambda: None)
 
 automata=DFA({"a","b","c","d","e"}, #estados
              {"alf":alfabeto,"dig":digito,"op":operador,"de":delimitador}, #abecedario
@@ -38,7 +48,6 @@ automata=DFA({"a","b","c","d","e"}, #estados
              "a", #estado inicial
              {"b","c","e","f","h","i"} #estados de aceptacion
              );
-
 buf1=0
 while(buf1<len(buf) and buf[buf1]!=' '):
     buf2=buf1
@@ -48,6 +57,13 @@ while(buf1<len(buf) and buf[buf1]!=' '):
         buf2=buf2+1
         lastState=state
         state=automata.changeState(state,buf[buf2])
-    buf1=buf2
-    print(lastState)
-        
+    if(lastState not in automata.F):
+        print("error")
+        break
+    else:
+        t=Token(lastState)();
+        val=buf[buf1:buf2]
+        stable.addSymbol(t,val)
+        buf1=buf2
+    
+print(stable.getTable())
--- analysers/symbolTable.py
+++ analysers/symbolTable.py
@@ -1,9 +1,9 @@
-class symbolTable:
+class symbolTableGlobal:
     def __init__(self,table):
         self.table=table
 
     def getTable(self):
         return self.table
 
-    def addSymbol(self,token,value):
-        self.table=self.table,{token:value}
+    def addSymbol(self,token,value=''):
+        self.table=self.table | {value:token}
